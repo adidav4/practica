6ee8549bf6f22522eb4c4902fcd42f4578cf9c78
adidav4 <davidadrian663@yahoo.com>
2023-06-29 19:00:33 +0300

    login si register

---
+++ frontend/src/login.jsx
@@ -0,0 +1,10 @@
+export const Login = () =>{
+    return(
+        <form action="">
+            <label for="email">Email</label>
+            <input type="email" placeholder="Email" id="email" name="email" />
+            <label for="password">Password</label>
+            <input type="password" placeholder="password" id="password" name="password" />
+        </form>
+    )
+}
